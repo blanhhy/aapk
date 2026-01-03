ba58a92da5691bb98f88f48775fbe251dc5cb257
繁Shrベラン <balancestar676@qq.com>
2026-01-03 14:06:09 +0800

    修复安装脚本的问题

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
-TARGET_PATH="/usr/local/bin/"
+TARGET_PATH="/usr/local/bin"
 SOURCE_PATH=$(dirname "$0")
 
-CMD="cp $SOURCE_PATH/aapk.sh $TARGET_PATH"
-
+CMD="cp $SOURCE_PATH/aapk.sh $TARGET_PATH/aapk"
 echo -e "Running \e[32m\"$CMD\"\e[0m"
+
+$CMD || exit 1
+
+CMD="chmod +x $TARGET_PATH/aapk"
+echo -e "Running \e[32m\"$CMD\"\e[0m"
+
 $CMD && echo "Successfully installed!"
